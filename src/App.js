04a051a0675047ea6ce0ef671346e893adb3f6b9
g100rabh <gsaurabh909@gmai.com>
2023-07-03 17:04:59 +0530

    Addressing security and logout issues

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import { useContext } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import TokenContext from "./store/token-context";
-import TokenProvider from "./store/TokenProvider";
 
 function App() {
-  
   const tokenCtx = useContext(TokenContext);
-
-  if(tokenCtx.isLoggedIn){}
+  console.log(tokenCtx.isLoggedIn);
   return (
-    <TokenProvider>
-      <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        {!tokenCtx.isLoggedIn && (
           <Route path="/auth">
             <AuthPage />
           </Route>
-         <Route path="/profile">
-            <UserProfile />
-          </Route>
-        </Switch>
-      </Layout>
-    </TokenProvider>
+        )}
+        <Route path="/profile">
+          {tokenCtx.isLoggedIn && <UserProfile />}
+          {!tokenCtx.isLoggedIn && <Redirect to="/auth" />}
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/store/TokenProvider.js
+++ src/store/TokenProvider.js
@@ -5,14 +5,21 @@ import TokenContext from "./token-context";
 const TokenProvider = (props) => {
   const [token, setToken] = useState(null);
 
+  if (token === null && localStorage.length !== 0) {
+    setToken(localStorage["user"]);
+  }
+  console.log(token)
+
   const userIsLoggedIn = !!token;
 
   const loginHandler = (tokenId) => {
-    setToken(tokenId);
+      setToken(tokenId);
+      localStorage.setItem("user", tokenId);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("user")
   };
 
   const tokenContext = {
@@ -21,7 +28,7 @@ const TokenProvider = (props) => {
     login: loginHandler,
     logout: logoutHandler,
   };
-  console.log(tokenContext.token);
+  // console.log(tokenContext.token);
   return (
     <TokenContext.Provider value={tokenContext}>
       {props.children}
